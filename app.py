26f199f3c9233089ce8f752776b17754cc699719
Patrick Cadell <cadell.patrick@gmail.com>
2019-11-20 14:18:29 -0600

    pip3 setup

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify, g
+
+from flask_cors import CORS
+
+from flask_login import LoginManager
+
+DEBUG = True
+PORT = 8000
+
+app = flask(__name__)
+
+@app.before_request
+def before_request():
+	g.db = models.DATABASE
+	g.db.connect()
+
+@app.after_request
+def after_request(response):
+	g.db.close()
+	return response
+
+@app.route('/test')
+def test_route():
+	return ['Heres the test route']
+
+if __name__ == '__main__':
+	models.initialize()
+	app.run(debug=DEBUG, port=PORT)
