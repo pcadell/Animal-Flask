71f92c71f9560c504b8cad90d8af3114e7c59254
Ken Swan <Thekenswan@gmail.com>
2019-11-20 15:37:23 -0600

    albums post route

--- resources/albums.py
+++ resources/albums.py
@@ -7,7 +7,7 @@ from playhouse.shortcuts import model_to_dict
 
 albums = Blueprint('albums', 'albums')
 
-@albums.route('/' methods=["GET"])
+@albums.route('/', methods=["GET"])
 def albums_index():
 	try: 
 		albums = [model_to_dict(albums) for albums in models.Album.select()]
@@ -15,3 +15,13 @@ def albums_index():
 		return jsonify(data=albums, status={"code": 200, "message": "Success"})
 	except models.DoesNotExist:
 		return jsonify(data={}, status={"code": 401, "message": "error getting the resources"})
+
+@albums.route('/', methods=["POST"])
+
+def create_albums():
+	payload = request.get_json()
+	album = models.Album.create(**payload)
+	print(album.__dict__)
+	print(dir(album))
+	album_dict = model_to_dict(album)
+	return jsonify(data=album_dict, status={"code": 201, "message": "Success"})
